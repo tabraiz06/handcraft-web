83edf6b3f21fb3af84a2a6b8f08ce5406bdb244f
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-12-01 18:21:15 +0530

    update in index file

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,16 @@ const app = express();
 
 app.use(express.json());
 app.use(cors())
+
+
+app.get("/", (req, res) => {
+  res.send("welcome to handcraft backend");
+});
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/orders", orderRoutes);
 
-app.get('/',(req,res)=>{
-    res.send("welcome to handcraft backend")
-})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
